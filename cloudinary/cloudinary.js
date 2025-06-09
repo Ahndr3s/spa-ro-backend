efa6afd8c984970bb1a07628bce109e7173b107e
Andres Jacinto <ahndrsx@gmail.com>
2025-06-09 15:23:26 -0600

    image uploading correction pt.6

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 
 // Configuration
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDNAME, 
-    api_key: process.env.CLOUDAPIKEY, 
-    api_secret: process.env.CLOUDAPISECRET,
-    upload_preset: process.env.UPLOADPRESET// Click 'View Credentials' below to copy your API secret
+    cloud_name: `${process.env.CLOUDNAME}`, 
+    api_key: `${process.env.CLOUDAPIKEY}`, 
+    api_secret:`${process.env.CLOUDAPISECRET}`,
+    upload_preset: `${process.env.UPLOADPRESET}`// Click 'View Credentials' below to copy your API secret
 });
 
 module.exports = cloudinary
